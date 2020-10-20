f9dde85cb2de0907d8f24eaecd6adc91a9d28bd1
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-10-19 22:06:18 -0400

    Updates

---
+++ precache-manifest.1a067d7ce0e015cc473a38da785deba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbefdc7387aea1cfba7f1653367614d",
+    "url": "/roundtrip-challenge/index.html"
+  },
+  {
+    "revision": "2fdccb2a945dc0718917",
+    "url": "/roundtrip-challenge/static/css/main.9f362dab.chunk.css"
+  },
+  {
+    "revision": "166d9b1c17b4e98e2bf9",
+    "url": "/roundtrip-challenge/static/js/2.930af2f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/roundtrip-challenge/static/js/2.930af2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdccb2a945dc0718917",
+    "url": "/roundtrip-challenge/static/js/main.9eb464a8.chunk.js"
+  },
+  {
+    "revision": "c7f18ec78bbc8f521363",
+    "url": "/roundtrip-challenge/static/js/runtime-main.2847ec09.js"
+  }
+]);
